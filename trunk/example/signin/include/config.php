18c44c248da0b510fdf75043b28149580d0f352d
felix021 <felix021@fba94542-9dec-11dd-b397-3d94ef37118f>
2008-12-12 05:52:01 +0000

    small adjustment in database.sql and final.doc

--- trunk/example/signin/include/config.php
+++ trunk/example/signin/include/config.php
@@ -3,7 +3,7 @@
 $dbhost = 'localhost'; //数据库服务器
 $dbuser = 'root'; //用户名
 $dbpass = '123456'; //密码
-$dbname = 'tst'; //数据库名
+$dbname = 'ttt'; //数据库名
 
 $adminuser = 'a'; //管理员用户名
 $adminpass = 'a'; //管理员密码
