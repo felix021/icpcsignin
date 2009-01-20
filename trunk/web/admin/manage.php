b1cde242d9d59d8791ac3ca2491ac042752b6589
felix021 <felix021@fba94542-9dec-11dd-b397-3d94ef37118f>
2009-01-20 13:36:13 +0000

    school management

--- trunk/web/admin/manage.php
+++ trunk/web/admin/manage.php
@@ -1,11 +1,20 @@
 <?php
 include("verify.php");
-include("header.php");
 ?>
-<p id="title">管理首页</p>
-<ul id="actions">
-<li><a href="index.php">退出登录</a></li>
-</ul>
-<?php
-include("footer.php");
-?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<style>
+a{color:blue;text-decoration:none;}
+a:hover{color:red;text-decoration:underline;}
+.tre{padding:2px;background-color:#eee;}
+.tro{padding:2px;background-color:#ddd;}
+.tblhead{padding:2px;color:#fff;background-color:#000;}
+td{text-align:center;padding:5px;}
+</style>
+</head>
+<frameset frameborder="1" cols="15%, 85%">
+<frame name="actions" src="actions.php" />
+<frame name="display" src="display.php" />
+</frameset>
+</html>
