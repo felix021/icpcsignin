4cdfd6ffccde829d6045586a738aae91e73b005f
felix021 <felix021@fba94542-9dec-11dd-b397-3d94ef37118f>
2009-02-17 14:10:35 +0000

    主页的结构修改

---
+++ web/article.php
@@ -0,0 +1,67 @@
+<?php
+//文章
+
+$itemsperpage = 5;
+$nowtime = time();
+$query = "SELECT COUNT(*) FROM `{tblprefix}_articles` WHERE `pub_time` < $nowtime";
+$res = getQuery($conn, $query);
+$row = $res->fetch_array();
+$articles_c = (int)$row[0];
+
+$pages_c = ceil($articles_c / $itemsperpage);
+$page = (int)$_GET['page'];
+
+$listbar = get_listbar($page, $articles_c, $itemsperpage, "index.php");
+echo <<<eot
+<div class="listbar">共{$pages_c}页{$articles_c}篇 $listbar</div>
+
+eot;
+
+$start = ($page - 1) * $itemsperpage;
+$query = "SELECT * FROM `{tblprefix}_articles` "
+        ."  WHERE `pub_time` < $nowtime"
+        ."  ORDER BY `priority` DESC, `pub_time` DESC"
+        ."  LIMIT $start, $itemsperpage";
+$res = getQuery($conn, $query);
+
+while($row = $res->fetch_assoc()){
+    extract($row, EXTR_OVERWRITE);
+    $title = htmlspecialchars($title);
+    $pubtime = time2str($pub_time, "Y-m-d H:i");
+    date_default_timezone_set("PRC");
+    if($pub_time > time()) continue; //定时发布文章, 不显示
+    if(time() - $pub_time < 3 * 86400) $newsign = '<span style="color:red;">[NEW!]</span>';
+    else $newsign = "";
+    if($permission == 0 && !isset($_SESSION['team_id']))
+        $content = 'Forbidden: 本文需要登陆后才可查看';
+    else{
+        $content = symbol2value($content);
+        switch($content_type){
+        case 0: //PLAIN
+            $content = htmlspecialchars($content);
+            $content = "<pre class=\"pre_content\">$content</pre>";
+            break;
+        case 1: //HTML
+            break;
+        case 2: //UBB
+        default:
+            $content = ubb2html($content);
+        }
+    }
+    echo <<<eot
+<div class="textbox">
+    <div class="textbox-title">[$pubtime] {$title} $newsign</div>
+    <div class="textbox-content">$content</div>
+</div>
+
+eot;
+}
+
+if($articles_c > 0){
+    echo <<<eot
+<div class="listbar">共{$pages_c}页{$articles_c}篇 $listbar</div>
+
+eot;
+}
+//end of articles
+?>
--- web/index.php
+++ web/index.php
@@ -10,7 +10,7 @@ echo <<<eot
 <tr>
 <td>
 <div id="header">
-title, or images.
+<img src="images/title.jpg" border="0"/>
     <div id="menu">
         <ul>
 <li><a href="index.php">首页</a></li>
@@ -27,6 +27,9 @@ echo <<<eot
 
 <li><a href="http://acm.whu.edu.cn/" target="_blank">WHUACM</a></li>
 <li><a href="http://acm.whu.edu.cn/woj" target="_blank">WOJ</a></li>
+<li><a href="index.php?page=hotel">宾馆</a></li>
+<li><a href="index.php?page=faq">FAQ</a></li>
+<li><a href="http://www.baidu.com/home.html" target="_blank">关于百度</a></li>
 
         </ul>
     </div>
@@ -35,74 +38,35 @@ echo <<<eot
     <div id="mainWrapper">
 
 eot;
-//文章
 
-$itemsperpage = 5;
-$nowtime = time();
-$query = "SELECT COUNT(*) FROM `{tblprefix}_articles` WHERE `pub_time` < $nowtime";
-$res = getQuery($conn, $query);
-$row = $res->fetch_array();
-$articles_c = (int)$row[0];
+switch($_GET['page']){
 
-$pages_c = ceil($articles_c / $itemsperpage);
-$page = (int)$_GET['page'];
+case 'hotel':
+    require_once("hotel.php");
+    break;
 
-$listbar = get_listbar($page, $articles_c, $itemsperpage, "index.php");
-echo <<<eot
-<div class="listbar">共{$pages_c}页{$articles_c}篇 $listbar</div>
+case 'map':
+    require_once("map.php");
+    break;
 
-eot;
+case 'faq':
+    require_once('faq.php');
+    break;
 
-$start = ($page - 1) * $itemsperpage;
-$query = "SELECT * FROM `{tblprefix}_articles` "
-        ."  WHERE `pub_time` < $nowtime"
-        ."  ORDER BY `priority` DESC, `pub_time` DESC"
-        ."  LIMIT $start, $itemsperpage";
-$res = getQuery($conn, $query);
-
-while($row = $res->fetch_assoc()){
-    extract($row, EXTR_OVERWRITE);
-    $title = htmlspecialchars($title);
-    $pubtime = time2str($pub_time, "Y-m-d H:i");
-    date_default_timezone_set("PRC");
-    if($pub_time > time()) continue; //定时发布文章, 不显示
-    if(time() - $pub_time < 3 * 86400) $newsign = '<span style="color:red;">[NEW!]</span>';
-    else $newsign = "";
-    if($permission == 0 && !isset($_SESSION['team_id']))
-        $content = 'Forbidden: 本文需要登陆后才可查看';
-    else{
-        $content = symbol2value($content);
-        switch($content_type){
-        case 0: //PLAIN
-            $content = htmlspecialchars($content);
-            $content = "<pre class=\"pre_content\">$content</pre>";
-            break;
-        case 1: //HTML
-            break;
-        case 2: //UBB
-        default:
-            $content = ubb2html($content);
-        }
-    }
-    echo <<<eot
-<div class="textbox">
-    <div class="textbox-title">[$pubtime] {$title} $newsign</div>
-    <div class="textbox-content">$content</div>
-</div>
-
-eot;
+case 'article':
+default:
+    require_once("article.php");    
 }
 
-if($articles_c > 0){
-    echo <<<eot
-<div class="listbar">共{$pages_c}页{$articles_c}篇 $listbar</div>
-
-eot;
-}
 
 echo <<<eot
         </div>
     </div>
+
+eot;
+
+//右列表
+echo <<<eot
 
     <div style="display: block;" id="sidebar" class="sidebar">
         <div id="innerSidebar">
@@ -118,7 +82,6 @@ echo <<<eot
 
 eot;
 
-//右列表
 if(isset($_SESSION['team_id'])){
     include(APP_ROOT."team_index.php");
 }else{
@@ -159,11 +122,20 @@ echo <<<eot
 </td>
 </tr>
 </table>
+<div id="ft">
+  <hr size="1" width="979">
+  <a href="http://www.whu.edu.cn" tppabs="http://www.whu.edu.cn/" target="_blank"><img border="0" src="images/whu.gif" tppabs="http://acm.whu.edu.cn/08cc/images/whu.gif" alt="武汉大学" width="181" height="50"></a><br>
+  Copyright © 2008 ACM/ICPC Association of Wuhan University. All rights reserved.<br>
+  Please <a class="link" href="mailto:acm@whu.edu.cn"><u><b>contact us</b></u><b></b></a> if you have any question.<br><br>
+</div>
 <hr width="1010"/>
 <script src="include/hl.js"></script>
 <script>
 highlighter();
 </script>
+</center>
+</body>
+</html>
 eot;
 include(APP_ROOT."include/footer.php");
 ?>
