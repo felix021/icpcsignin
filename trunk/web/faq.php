67b1026bd9444d6f337f05949669f5d0b1217155
felix021 <felix021@fba94542-9dec-11dd-b397-3d94ef37118f>
2009-02-17 14:10:35 +0000

    主页的结构修改

---
+++ trunk/web/faq.php
@@ -0,0 +1,4 @@
+<div class="textbox" style="margin-top:8px;">
+    <div class="textbox-title">FAQ</div>
+    <div class="textbox-content">FAQ Contents</div>
+</div>
---
+++ trunk/web/hotel.php
@@ -0,0 +1,4 @@
+<div class="textbox" style="margin-top:8px;">
+    <div class="textbox-title">Hotel</div>
+    <div class="textbox-content">Hotel Contents</div>
+</div>
