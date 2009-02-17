a1a5cd9b5a895309c8f90c9d79d3dc56b50e7ded
felix021 <felix021@fba94542-9dec-11dd-b397-3d94ef37118f>
2009-02-17 14:18:43 +0000

    队伍界面添加成员

--- web/include/phpmailer/class.mailer.php
+++ web/include/phpmailer/class.mailer.php
@@ -7,12 +7,12 @@ class mailer extends PHPMailer{
     public $Encoding    = 'base64';
     public $ContentType = 'text/html';
     public $CharSet     = 'UTF-8';
-    public $From        = 'acm@whu.edu.cn';
+    public $From        = 'whuacm2009@gmail.com';
     public $FromName    = 'whuacm';
 
     public function email($name, $address, $subject, $body){
         $this->IsSMTP();
-        $this->AddReplyTo('acm@whu.edu.cn', 'whuacm');
+        $this->AddReplyTo('whuacm2009@gmail.com', 'whuacm');
         $this->Subject = $subject;
         $this->Body    = $body;
         $this->AddAddress($address, $name);
