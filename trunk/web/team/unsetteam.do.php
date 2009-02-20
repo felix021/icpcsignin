e0f18896f13a6b288c7fd6d5e9ee0e0603815ae9
felix021 <felix021@fba94542-9dec-11dd-b397-3d94ef37118f>
2009-02-20 19:24:51 +0000

    (用户)删除队伍逻辑加强(要求输入密码)

--- trunk/web/team/unsetteam.do.php
+++ trunk/web/team/unsetteam.do.php
@@ -3,12 +3,21 @@ $relpath = dirname(__FILE__);
 include($relpath."/def.php");
 
 include(APP_ROOT."team/inc.php");
-$id = (int)$_SESSION['team_id'];
+$team_id = (int)$_SESSION['team_id'];
+$t = new team($team_id);
+if($t->errno) msgbox($t->error);
 
 $url = parse_url($_SERVER['HTTP_REFERER']);
 $ref = basename($url['path']);
+$pass = $_POST['password'];
+if(get_magic_quotes_gpc()){
+    $pass = stripslashes($pass);
+}
+if($pass != $t->password){
+    msgbox("请输入正确的密码！");
+}
 if($ref == "index.php" || $ref == "unsetteam.php"){
-    if (team::delById($id)){
+    if (team::delById($team_id)){
         unset($_SESSION['team_id']);
         include(APP_ROOT."include/header.php");
         $msg = <<<eot
--- trunk/web/team/unsetteam.php
+++ trunk/web/team/unsetteam.php
@@ -5,32 +5,20 @@ include($relpath."/def.php");
 include(APP_ROOT."team/inc.php");
 ?>
 <script language="javascript">
-function change(t){
-    var b = document.getElementById("delbtn");
-    if(b){
-        if (t.value == "OK" || t.value == "ok"){
-            b.disabled = false;
-        }else{
-            b.disabled = true;
-        }
-    }
-}
 function unsetteam(){
     if(confirm("确认要删除此队伍?删除后队伍信息将无法恢复!")){
         var unsetbtn = document.getElementById("unsetbtn");
         if(unsetbtn) unsetbtn.click();
-        //window.location = 'team/unsetteam.do.php';
     }
 }
 </script>
 <div class="textbox">
 <div class="textbox-title">删除队伍 [注意：删除后队伍信息不可恢复！]</div>
 <div class="textbox-content">
-请输入"OK"进行确认: 
-<input type="text" size="5" onkeyup="javascript:change(this)"/>
-<input type="button" value="确认删除" id="delbtn" disabled onclick="unsetteam()"/>
-<form action="team/unsetteam.do.php" style="display:none;" method="get">
-<input type="submit" id="unsetbtn">
+<form action="team/unsetteam.do.php" method="post">
+请输入队伍密码进行确认: <input type="password" name="password" size="10"/>
+<input type="button" value="确认删除" id="delbtn" onclick="unsetteam()"/>
+<input type="submit" id="unsetbtn" style="display:none;" />
 </form>
 </div>
 </div>
