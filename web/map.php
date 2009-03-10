ce1cd982bb22eefa1c1ffe2d29ef3f53547a5064
felix021 <felix021@fba94542-9dec-11dd-b397-3d94ef37118f>
2009-03-10 15:46:38 +0000

    修改赛场地图

--- web/map.php
+++ web/map.php
@@ -6,7 +6,7 @@
 B点为武汉大学计算机学院，即赛场所在地<br/>
 C点为武汉大学信息学部正门（又名"武测"，公交车站点名为“广埠屯”）
 <p style="text-align:center;">
-<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://local.google.com/maps?f=d&amp;source=s_d&amp;saddr=%E5%85%AB%E4%B8%80%E8%B7%AF&amp;daddr=30.525966,114.359994+to:%E7%8F%9E%E5%96%BB%E8%B7%AF&amp;hl=zh-CN&amp;geocode=FXTl0QEdAPbQBg%3B%3BFYzD0QEdNAPRBg&amp;mra=dme&amp;mrcr=0,1&amp;mrsp=1&amp;sz=15&amp;sll=30.527592,114.364543&amp;sspn=0.013234,0.030642&amp;ie=UTF8&amp;s=AARTsJocyLun_IEV7_be6pr22ULIyIE7tQ&amp;ll=30.528923,114.362097&amp;spn=0.017744,0.027466&amp;z=15&amp;output=embed"></iframe>
+<img src="images/map.png" border="0"/>
 </p>
 </div>
 </div>
